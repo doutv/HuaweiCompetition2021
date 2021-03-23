3c756a0931f50b9e9835302686a7641707c39486
Yongjin Huang <1765941460@qq.com>
2021-03-23 21:33:28 +0800

    搭建框架

--- SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
+++ SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
-
+#include <set>
 using namespace std;
 namespace common
 {
@@ -31,6 +31,7 @@ namespace common
 			res += c;
 			c = std::getchar();
 		}
+		return res;
 	}
 	inline void remove_lparen()
 	{
@@ -55,7 +56,7 @@ public:
 	int32_t num_of_server;
 	static const int32_t server_max_size = 100;
 	ServerInfo server[server_max_size];
-	unordered_map<string, ServerInfo> server_map;
+	unordered_map<string, ServerInfo *> servername2info_map;
 	void Init()
 	{
 		cin >> num_of_server;
@@ -67,7 +68,7 @@ public:
 			server[i].memory = common::read_int();
 			server[i].buy_cost = common::read_int();
 			server[i].daily_cost = common::read_int();
-			server_map.emplace(server[i].server_name, server[i]);
+			servername2info_map.emplace(server[i].server_name, &server[i]);
 		}
 	}
 
@@ -91,13 +92,13 @@ public:
 		std::string vm_name;
 		int32_t core;
 		int32_t memory;
-		bool deploy_type;
+		bool deploy_type; // 0 -> 单节点  1 -> 双节点
 	};
 
 	int32_t num_of_vm;
 	static const int32_t vm_max_size = 1000;
 	VMInfo vm[vm_max_size];
-	unordered_map<string, VMInfo> vmtype_map;
+	unordered_map<string, VMInfo *> vmname2info_map;
 	void Init()
 	{
 		cin >> num_of_vm;
@@ -108,7 +109,7 @@ public:
 			vm[i].core = common::read_int();
 			vm[i].memory = common::read_int();
 			vm[i].deploy_type = common::read_int();
-			vmtype_map.emplace(vm[i].vm_name, vm[i]);
+			vmname2info_map.emplace(vm[i].vm_name, &vm[i]);
 		}
 	}
 
@@ -130,7 +131,7 @@ public:
 	int32_t num_of_day;
 	struct Req
 	{
-		bool add_or_delete;
+		bool req_type; // 0 -> delete 1 -> add
 		std::string vm_name;
 		int32_t vm_id;
 	};
@@ -148,16 +149,16 @@ public:
 			{
 				Req cur_req;
 				common::remove_lparen();
-				string add_or_delete_str = common::get_string();
-				if (add_or_delete_str == "add")
+				string req_type_str = common::get_string();
+				if (req_type_str == "add")
 				{
-					cur_req.add_or_delete = 1;
+					cur_req.req_type = 1;
 					cur_req.vm_name = common::get_string();
 					cur_req.vm_id = common::read_int();
 				}
 				else
 				{
-					cur_req.add_or_delete = 0;
+					cur_req.req_type = 0;
 					cur_req.vm_id = common::read_int();
 				}
 				today_req.push_back(cur_req);
@@ -172,7 +173,7 @@ public:
 		{
 			for (auto cur_req : day_req)
 			{
-				cout << cur_req.add_or_delete << " " << cur_req.vm_name << " " << cur_req.vm_id << endl;
+				cout << cur_req.req_type << " " << cur_req.vm_name << " " << cur_req.vm_id << endl;
 			}
 		}
 	}
@@ -182,39 +183,114 @@ public:
 
 class RunningVM
 {
+	// 当前在运行的 vm 序列
 public:
-	struct VMInfo
+	const VirtualMachine &vm;
+	unordered_map<int32_t, VirtualMachine::VMInfo *> vmid2info_map;
+	set<int32_t> vmid_set;
+	const VirtualMachine::VMInfo GetVMInfoById(int32_t vm_id)
 	{
-		std::string vm_name;
-		int32_t core;
-		int32_t memory;
-		bool deploy_type;
-	};
-	unordered_map<int32_t, VMInfo> vmid_map;
+		return *vmid2info_map.at(vm_id);
+	}
+	void AddVM(int32_t vm_id, VirtualMachine::VMInfo *vm_info)
+	{
+		vmid_set.emplace(vm_id);
+		vmid2info_map.emplace(vm_id, vm_info);
+	}
+	void DelVM(int32_t vm_id)
+	{
+		vmid_set.erase(vm_id);
+	}
 
-	void init()
-	{
-	}
-	int64_t evaluate_cost()
-	{
-	}
-	RunningVM(){};
+	RunningVM(VirtualMachine &vm_) : vm(vm_) {}
 };
+
+class BoughtServer
+{
+	// 已经购买的 server
+	// 可能处于关机状态
+public:
+	const Server &server;
+	unordered_map<int32_t, Server::ServerInfo *> serverid2info_map;
+	set<int32_t> serverid_set;
+	const Server::ServerInfo GetServerInfoById(const int32_t server_id)
+	{
+		return *serverid2info_map.at(server_id);
+	}
+	void AddServer(const int32_t server_id, const Server::ServerInfo *server_info)
+	{
+		serverid_set.emplace(server_id);
+		serverid2info_map.emplace(server_id, server_info);
+	}
+	int64_t EvaluateBuyingCost()
+	{
+		int64_t buying_cost = 0;
+		for (const int32_t &server_id : serverid_set)
+		{
+			buying_cost += GetServerInfoById(server_id).buy_cost;
+		}
+		return buying_cost;
+	}
+	BoughtServer(Server &server_) : server(server_) {}
+};
+class Greedy
+{
+public:
+	const Server &server;
+	const VirtualMachine &vm;
+	const VMRequest &request;
+	RunningVM &running_vm;
+	BoughtServer &bought_server;
+	vector<Server::ServerInfo> server_list;
+	unordered_map<int32_t, Server::ServerInfo *> vm2server_map;
+	set<int32_t> running_serverid_set;
+
+	
+	void buy_all_server_at_first()
+	{
+		for (auto day_req : request.req)
+		{
+			for (auto each_req : day_req)
+			{
+				// skip del type
+				if (each_req.req_type == 0)
+					continue;
+				const VirtualMachine::VMInfo cur_vm = *vm.vmname2info_map.at(each_req.vm_name);
+			}
+		}
+	}
+	void simulate()
+	{
+
+		buy_all_server_at_first();
+	}
+	Greedy(const Server &server_, const VirtualMachine &vm_, const VMRequest &request_,
+		   RunningVM &rvm_, BoughtServer &rserver_)
+		: server(server_), vm(vm_), request(request_), run_vm(rvm_), bought_server(rserver_) {}
+};
+
 int main()
 {
 #ifdef DEBUG
 	// const char *test_file_path = "../training1.txt";
 	// freopen(test_file_path, "r", stdin);
 #endif
+	// 读取所有输入数据
 	Server server;
+	VirtualMachine vm;
+	VMRequest request;
 	server.Init();
-	VirtualMachine VM;
-	VM.Init();
-	VMRequest Request;
-	Request.Init();
+	vm.Init();
+	request.Init();
 #ifdef DEBUG
 	server.PrintInfo();
-	VM.PrintInfo();
+	vm.PrintInfo();
+	request.PrintInfo();
 #endif
+	// 贪心
+	RunningVM running_vm(vm);
+	BoughtServer bought_server(server);
+	Greedy greedy(server, vm, request, running_vm, bought_server);
+	greedy.simulate();
 	return 0;
 }
