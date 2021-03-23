d38f07d52a21f36016adfcbc388e113a60f0dcf9
Yongjin Huang <1765941460@qq.com>
2021-03-23 11:23:44 +0800

    测试cmake

--- SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
+++ SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
@@ -1,11 +1,11 @@
 #include "iostream"
-
+using namespace std;
 int main()
 {
 	// TODO:read standard input
 	// TODO:process
 	// TODO:write standard output
 	// TODO:fflush(stdout);
-
+	std::cout << "test bulid" << std::endl;
 	return 0;
 }
