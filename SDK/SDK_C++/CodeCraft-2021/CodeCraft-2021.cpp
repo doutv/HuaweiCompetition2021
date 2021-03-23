2eb544d5b87ff1f2c383b20c1d8038ef89d4c10a
Yongjin Huang <1765941460@qq.com>
2021-03-23 22:56:56 +0800

    明天再干！

--- SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
+++ SDK/SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
@@ -71,7 +71,10 @@ public:
 			servername2info_map.emplace(server[i].server_name, &server[i]);
 		}
 	}
-
+	const ServerInfo &GetServerInfoByName(const string &server_name) const
+	{
+		return *servername2info_map.at(server_name);
+	}
 	void PrintInfo()
 	{
 		for (int32_t i = 0; i < num_of_server; ++i)
@@ -112,7 +115,10 @@ public:
 			vmname2info_map.emplace(vm[i].vm_name, &vm[i]);
 		}
 	}
-
+	const VMInfo &GetVMInfoByName(const string &vm_name) const
+	{
+		return *vmname2info_map.at(vm_name);
+	}
 	void PrintInfo()
 	{
 		for (int32_t i = 0; i < num_of_vm; ++i)
@@ -188,7 +194,7 @@ public:
 	const VirtualMachine &vm;
 	unordered_map<int32_t, VirtualMachine::VMInfo *> vmid2info_map;
 	set<int32_t> vmid_set;
-	const VirtualMachine::VMInfo GetVMInfoById(int32_t vm_id)
+	const VirtualMachine::VMInfo &GetVMInfoById(int32_t vm_id)
 	{
 		return *vmid2info_map.at(vm_id);
 	}
@@ -202,7 +208,7 @@ public:
 		vmid_set.erase(vm_id);
 	}
 
-	RunningVM(VirtualMachine &vm_) : vm(vm_) {}
+	RunningVM(const VirtualMachine &vm) : vm(vm) {}
 };
 
 class BoughtServer
@@ -213,60 +219,111 @@ public:
 	const Server &server;
 	unordered_map<int32_t, Server::ServerInfo *> serverid2info_map;
 	set<int32_t> serverid_set;
-	const Server::ServerInfo GetServerInfoById(const int32_t server_id)
+	const Server::ServerInfo &GetServerInfoById(const int32_t server_id)
 	{
 		return *serverid2info_map.at(server_id);
 	}
-	void AddServer(const int32_t server_id, const Server::ServerInfo *server_info)
+	int64_t BuyServer(const int32_t server_id, Server::ServerInfo *server_info)
 	{
 		serverid_set.emplace(server_id);
 		serverid2info_map.emplace(server_id, server_info);
+		return server_info->buy_cost;
 	}
-	int64_t EvaluateBuyingCost()
-	{
-		int64_t buying_cost = 0;
-		for (const int32_t &server_id : serverid_set)
-		{
-			buying_cost += GetServerInfoById(server_id).buy_cost;
-		}
-		return buying_cost;
-	}
-	BoughtServer(Server &server_) : server(server_) {}
+	BoughtServer(const Server &server) : server(server) {}
 };
 class Greedy
 {
 public:
 	const Server &server;
-	const VirtualMachine &vm;
+	const VirtualMachine &virtual_machine;
 	const VMRequest &request;
-	RunningVM &running_vm;
-	BoughtServer &bought_server;
+	RunningVM running_vm;
+	BoughtServer bought_server;
+
 	vector<Server::ServerInfo> server_list;
 	unordered_map<int32_t, Server::ServerInfo *> vm2server_map;
 	set<int32_t> running_serverid_set;
-
-	
-	void buy_all_server_at_first()
+	set<int32_t> sleeping_serverid_set;
+	int64_t total_cost;
+	int32_t today;
+	// void BuyAllServerAtFirst()
+	// {
+	// 	for (auto day_req : request.req)
+	// 	{
+	// 		for (auto each_req : day_req)
+	// 		{
+	// 			// skip del type
+	// 			if (each_req.req_type == 0)
+	// 				continue;
+	// 			const VirtualMachine::VMInfo &cur_vm = *vm.vmname2info_map.at(each_req.vm_name);
+	// 		}
+	// 	}
+	// }
+	int64_t EvaluateDailyCost()
 	{
-		for (auto day_req : request.req)
+		int64_t daily_cost = 0;
+		for (int32_t server_id : running_serverid_set)
 		{
-			for (auto each_req : day_req)
+			daily_cost += bought_server.GetServerInfoById(server_id).daily_cost;
+		}
+		return daily_cost;
+	}
+	void AssignServer2VM(int32_t server_id, const VirtualMachine::VMInfo &vm)
+	{
+	}
+	bool TryAssignSleepingServer2VM(const VirtualMachine::VMInfo &vm)
+	{
+		for (int32_t server_id : sleeping_serverid_set)
+		{
+			const Server::ServerInfo &server_info = bought_server.GetServerInfoById(server_id);
+			if (server_info.core >= vm.core && server_info.memory >= vm.memory)
 			{
-				// skip del type
-				if (each_req.req_type == 0)
-					continue;
-				const VirtualMachine::VMInfo cur_vm = *vm.vmname2info_map.at(each_req.vm_name);
+				// greedy assign sleeping server to vm
+				AssignServer2VM(server_id, vm);
+				sleeping_serverid_set.erase(server_id);
+				running_serverid_set.emplace(server_id);
+				return true;
 			}
 		}
+		return false;
 	}
-	void simulate()
+	void BuyServerBasedOnVM(const VirtualMachine::VMInfo &vm)
 	{
-
-		buy_all_server_at_first();
 	}
-	Greedy(const Server &server_, const VirtualMachine &vm_, const VMRequest &request_,
-		   RunningVM &rvm_, BoughtServer &rserver_)
-		: server(server_), vm(vm_), request(request_), run_vm(rvm_), bought_server(rserver_) {}
+	void Buy()
+	{
+		// total_cost += bought_server.BuyServer();
+		for (const VMRequest::Req &cur_req : request.req.at(today))
+		{
+			// skip del type
+			if (cur_req.req_type == 0)
+				continue;
+			const VirtualMachine::VMInfo &cur_vm = virtual_machine.GetVMInfoByName(cur_req.vm_name);
+			if (!TryAssignSleepingServer2VM(cur_vm))
+				BuyServerBasedOnVM(cur_vm);
+		}
+	}
+	void Migrate()
+	{
+	}
+	void ProcessRequest()
+	{
+	}
+	void Simulate()
+	{
+		// 对于每一天的操作，会先按顺序执行选手输出的购买操作，然后按顺序执行选手输出的迁移操作，最后按顺序执行当天的创建和删除操作。
+		// 输入中可能存在当天创建后当天删除的虚拟机，对于这些虚拟机请求，你依然需要按顺序正常处理。
+		// BuyAllServerAtFirst();
+		for (today = 0; today < request.num_of_day; today++)
+		{
+			Buy();
+			Migrate();
+			ProcessRequest();
+			total_cost += EvaluateDailyCost();
+		}
+	}
+	Greedy(const Server &server, const VirtualMachine &virtual_machine, const VMRequest &request)
+		: server(server), virtual_machine(virtual_machine), request(request), running_vm(virtual_machine), bought_server(server) {}
 };
 
 int main()
@@ -277,20 +334,18 @@ int main()
 #endif
 	// 读取所有输入数据
 	Server server;
-	VirtualMachine vm;
+	VirtualMachine virtual_machine;
 	VMRequest request;
 	server.Init();
-	vm.Init();
+	virtual_machine.Init();
 	request.Init();
 #ifdef DEBUG
 	server.PrintInfo();
-	vm.PrintInfo();
+	virtual_machine.PrintInfo();
 	request.PrintInfo();
 #endif
 	// 贪心
-	RunningVM running_vm(vm);
-	BoughtServer bought_server(server);
-	Greedy greedy(server, vm, request, running_vm, bought_server);
-	greedy.simulate();
+	Greedy greedy(server, virtual_machine, request);
+	greedy.Simulate();
 	return 0;
 }
